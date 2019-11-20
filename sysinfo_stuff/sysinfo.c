7ea8fa047dccfbca73d902cb9b644637fbbfbe80
Chris Cohen <officers@sigapp.club>
2019-11-20 13:52:47 -0500

    removed bash script, now system info is parsed using C

--- sysinfo_stuff/sysinfo.c
+++ sysinfo_stuff/sysinfo.c
@@ -1,44 +1,181 @@
+#include "sysinfo.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void get_system_info() {
-  /* Execute bash script to parse system info and read in resulting file. */
+#define KB_TO_GB (1048576)
+#define MAX_INT_SIZE (20)
 
-  system("./get_sysinfo.sh > sysinfo.txt");
-  FILE * fp = fopen("./sysinfo.txt", "r");
+system_info g_sys_info = { 0 };
+
+void get_release_info() {
+  /* OS RELEASE INFORMATION */
+
+  FILE * fp = fopen("/etc/os-release", "r");
   if (fp == NULL) {
-    perror("Couldn't read sysinfo.txt");
+    perror("couldn't read /etc/os-release");
     exit(EXIT_FAILURE);
   }
 
-  /* find file size */
-
-  fseek(fp, 0, SEEK_END);
-  long fsize = ftell(fp);
-  fseek(fp, 0, SEEK_SET);
-
-  char * buffer = malloc(sizeof(char) * fsize);
-
-  fread(buffer, sizeof(char), fsize, fp);
-  /*
-  if (fread(buffer, sizeof(char), fsize, fp) <= fsize) {
-    perror("fread");
-    free(buffer);
-    buffer = NULL;
-    exit(EXIT_FAILURE);
+  char * line = NULL;
+  size_t len = 0;
+  ssize_t read_len = 0;
+  while ((read_len = getline(&line, &len, fp)) != -1) {
+    if (strstr(line, "NAME=") != NULL) {
+      char * first_quote = strchr(line, '"');
+      char * second_quote = strchr(first_quote + 1, '"');
+      g_sys_info.release_name = strndup(first_quote + 1,
+                                        second_quote - first_quote - 1);
+      if (g_sys_info.release_version != NULL) {
+        /* if release version already found too, we need no other information */
+        break;
+      }
+    }
+    else if (strstr(line, "VERSION=") != NULL) {
+      char * first_quote = strchr(line, '"');
+      char * second_quote = strchr(first_quote + 1, '"');
+      g_sys_info.release_version = strndup(first_quote + 1,
+                                           second_quote - first_quote - 1);
+      if (g_sys_info.release_name != NULL) {
+        /* if release name already found too, we need no other information */
+        break;
+      }
+    }
   }
-  */
-
-  /* Parse each line to get system info needed */
-  // RELEASE
-
-  printf("%s\n", buffer);
 
   fclose(fp);
   fp = NULL;
-} /* get_system_info() */
+  free(line);
+  line = NULL;
 
-int main() {
-  get_system_info();
+  /* KERNEL INFORMATION */
+
+  fp = fopen("/proc/version_signature", "r");
+  if (fp == NULL) {
+    perror("couldn't read /proc/version_signature");
+    exit(EXIT_FAILURE);
+  }
+
+  if ((read_len = getline(&line, &len, fp)) != -1) {
+    g_sys_info.kernel_version = strdup(strstr(line, " ") + 1);
+  }
+  else {
+    perror("couldn't read /proc/version_signature");
+    exit(EXIT_FAILURE);
+  }
+
+  fclose(fp);
+  fp = NULL;
+  free(line);
+  line = NULL;
+} /* get_release_info() */
+
+void get_hardware_info() {
+  /* RAM information */
+  FILE * fp = fopen("/proc/meminfo", "r");
+  if (fp == NULL) {
+    perror("couldn't read /proc/meminfo");
+    exit(EXIT_FAILURE);
+  }
+
+
+  char * line = NULL;
+  size_t len = 0;
+  ssize_t read_len = 0;
+  while ((read_len = getline(&line, &len, fp)) != -1) {
+    if (strstr(line, "MemTotal") != NULL) {
+      char * start_mem_string = strchr(line, ' ');
+      char * end_mem_string = strchr(line + 1, ' ');
+      char * mem_string = strndup(start_mem_string + 1,
+                                  end_mem_string - start_mem_string - 1);
+      long int total_mem_kb = strtol(mem_string, NULL, 10);
+      g_sys_info.ram_size = malloc(sizeof(char) * MAX_INT_SIZE);
+      sprintf(g_sys_info.ram_size, "%li", total_mem_kb);
+      /*
+      long int total_mem_gb = total_mem_kb / KB_TO_GB;
+      sprintf(g_sys_info.ram_size, "%li", total_mem_gb);
+      */
+
+      free(mem_string);
+      mem_string = NULL;
+      break;
+    }
+  }
+
+  fclose(fp);
+  fp = NULL;
+  free(line);
+  line = NULL;
+
+  fp = fopen("/proc/cpuinfo", "r");
+  if (fp == NULL) {
+    perror("error reading /proc/cpuinfo");
+    exit(EXIT_FAILURE);
+  }
+
+  char * cpu_name = NULL;
+  char * num_cores = NULL;
+  while ((read_len = getline(&line, &len, fp)) != -1) {
+    if (strstr(line, "model name") != NULL) {
+      if (cpu_name == NULL) {
+        cpu_name = strndup(strchr(line, ':') + 2, strlen(strchr(line, ':') + 2) - 1);
+      }
+    }
+    else if (strstr(line, "cpu cores") != NULL) {
+      if (num_cores == NULL) {
+        num_cores = strdup(strchr(line, ':') + 2);
+      }
+    }
+  }
+
+  cpu_name = realloc(cpu_name, strlen(cpu_name) + strlen(num_cores) + 5);
+  strcat(cpu_name, " x ");
+  strcat(cpu_name, num_cores);
+  g_sys_info.cpu_info = strdup(cpu_name);
+
+  free(cpu_name);
+  cpu_name = NULL;
+  free(num_cores);
+  num_cores = NULL;
+  fclose(fp);
+  fp = NULL;
+  free(line);
+  line = NULL;
+} /* get_hardware_info() */
+
+void get_disk_info() {
+// mounts
+} /* get_disk_info() */
+
+void main() {
+  get_release_info();
+  get_hardware_info();
+  //get_disk_info();
+
+  printf("---------- RELEASE ----------\n");
+  printf("%s\n", g_sys_info.release_name);
+  printf("%s\n", g_sys_info.release_version);
+  printf("%s\n", g_sys_info.kernel_version);
+
+  printf("---------- HARDWARE ----------\n");
+  printf("%s\n", g_sys_info.ram_size);
+  printf("%s\b", g_sys_info.cpu_info);
+
+  printf("---------- DISK ----------\n");
+  //printf("%s\n", g_sys_info.disk_space);
+  free(g_sys_info.release_name);
+  g_sys_info.release_name = NULL;
+  free(g_sys_info.release_version);
+  g_sys_info.release_version = NULL;
+  free(g_sys_info.kernel_version);
+  g_sys_info.kernel_version = NULL;
+  free(g_sys_info.ram_size);
+  g_sys_info.ram_size = NULL;
+  free(g_sys_info.cpu_info);
+  g_sys_info.cpu_info = NULL;
+  /*
+  free(g_sys_info.disk_space);
+  g_sys_info.disk_space = NULL;
+  */
 } /* main() */
