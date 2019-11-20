b8f52a157f766081f85c671ba24c6b382abf376d
Cora YuNing Chan <chan207@data.cs.purdue.edu>
2019-11-20 13:33:25 -0500

    Tabs created

--- example_0.c
+++ example_0.c
@@ -2,10 +2,52 @@
 
 static void activate(GtkApplication *app, gpointer user_data) {
   GtkWidget *window;
+  GtkWidget *button;
+  GtkWidget *grid;
+  GtkWidget *notebook;
+  GtkWidget *frame;
+  GtkWidget *label;
 
+  // create new window
   window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size(GTK_WINDOW (window), 200, 200);
+  gtk_window_set_title(GTK_WINDOW (window), "System Monitor");
+  gtk_window_set_default_size(GTK_WINDOW (window), 600, 600);
+
+  // create a new notebook
+  notebook = gtk_notebook_new();
+  gtk_container_add(GTK_CONTAINER (window), notebook);
+
+  // set tab position
+  gtk_notebook_set_tab_pos(GTK_NOTEBOOK (notebook), GTK_POS_TOP);
+
+  // create System frame and label
+  frame = gtk_frame_new("Info");
+  label = gtk_label_new("System");
+
+  // add page
+  gtk_notebook_append_page(GTK_NOTEBOOK (notebook), frame, label);
+
+  // create Processes frame and label
+  frame = gtk_frame_new("Info");
+  label = gtk_label_new("Processes");
+
+  // add page
+  gtk_notebook_append_page(GTK_NOTEBOOK (notebook), frame, label);
+  
+  // create Resources frame and label
+  frame = gtk_frame_new("Info");
+  label = gtk_label_new("Resources");
+
+  // add page
+  gtk_notebook_append_page(GTK_NOTEBOOK (notebook), frame, label);
+
+  // create File Systems frame and label
+  frame = gtk_frame_new("Info");
+  label = gtk_label_new("File Systems");
+
+  // add pages
+  gtk_notebook_append_page(GTK_NOTEBOOK (notebook), frame, label);
+
   gtk_widget_show_all(window);
 }
 
